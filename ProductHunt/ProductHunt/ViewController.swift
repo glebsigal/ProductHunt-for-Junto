9b6b3c1b0ae32015fcd01c787f07711c76a7d77b
Gleb Sigal <glebsigal@MacBook-Air-Gleb.local>
2017-08-19 01:18:43 +0300

    initial commit

---
+++ ProductHunt/ProductHunt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProductHunt
+//
+//  Created by Gleb Sigal on 19.08.17.
+//  Copyright © 2017 Gleb Sigal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
